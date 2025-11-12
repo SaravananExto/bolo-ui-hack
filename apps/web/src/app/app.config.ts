f84a232e7cf583f016c58df22eba5e11a9d3d602
Pavan Kumar D <pavankumar.d@exto360.com>
2025-11-12 11:04:08 +0530

    style(primeng): added primeng configs globally

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -4,6 +4,9 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Material from '@primeuix/themes/material';
+import { providePrimeNG } from 'primeng/config';
 
 import { appRoutes } from './app.routes';
 
@@ -12,5 +15,19 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimationsAsync(), // this is deprecated but needed for primeng animations
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Material,
+      },
+      inputVariant: 'filled',
+      zIndex: {
+        modal: 1100,
+        overlay: 1000,
+        menu: 1000,
+        tooltip: 1100,
+      },
+    }),
   ],
 };
